01e7ea67f9f327927aacbdf41080c65bdd2dc24f
Crowdbotics <team@crowdbotics.com>
2021-04-06 01:07:53 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/scribe_mobile_25484/MainActivity.java
@@ -0,0 +1,15 @@
+package com.scribe_mobile_25484;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "scribe_mobile_25484";
+    }
+}
---
+++ src/config/app.js
@@ -0,0 +1,5 @@
+export const appConfig = {
+  // todo add library to handle env variables
+  emailAuthAPIEndPoint: "https://scribe-mobile-25484.botics.co",
+  defaultTimeout: 5000
+};
